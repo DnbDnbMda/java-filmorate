47a1969a568f60984ffca322a5774fd66b4eecd3
d.molchanov <dnb2011@yandex.ru>
2023-05-18 23:47:14 +0300

    fix: Error:   WhitespaceAround: '+' is not followed by whitespace. Empty blocks may only be represented as {} when not part of a multi-block statement (4.1.3)

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -46,7 +46,7 @@ public class User {
     }
 
     private int generateIdUser() {
-        int maxId = UserController.getMaxIdUser()+1;
-        return maxId;
+        int maxId = UserController.getMaxIdUser();
+        return maxId++;
     }
 }
