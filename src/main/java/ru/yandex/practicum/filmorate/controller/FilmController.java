54e2381ca89939d74e4a247fb17d28bf8e0c414b
d.molchanov <dnb2011@yandex.ru>
2023-05-18 23:08:41 +0300

    feat: реализован контроллер для обновления фильма и проверки вынесены в отдельный метод

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,10 +1,7 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidEx;
 import ru.yandex.practicum.filmorate.model.Film;
 
@@ -19,23 +16,24 @@ import java.util.List;
 @RestController
 public class FilmController {
     private final HashMap<Integer, Film> films = new HashMap<>();
+    private final static Date LIMITE_DATE = Date.from(Instant.parse("1895-12-28T00:00:00.00Z"));
 
-    @PutMapping("/films")
+    @PostMapping("/films")
     public Film putFilm(@Valid @RequestBody Film film) throws ValidEx {
-        Date limitDate = Date.from(Instant.parse("1895-12-28T00:00:00.00Z"));
-        if (film.getName().isBlank()) {
-            throw new ValidEx("Название фильма не может быть пустым");
-        } else if (film.getDescription().length() > 200) {
-            throw new ValidEx("Описание фильма не может превышать 200 знаков");
-        } else if (film.getRealeseDate().after(limitDate) | !film.getRealeseDate().equals(limitDate)) {
-            throw new ValidEx("Дата релиза фильма не может быть раньше 28 декабря 1895 года");
-        } else if (film.getDuration().isNegative() | film.getDuration().isZero()) {
-            throw new ValidEx("Длительность фильма должна быть больше нуля");
-        } else {
+        if (validateFilmData(film)) {
             films.put(film.getId(), film);
             log.info("Добавлен фильм");
             return film;
-        }
+        } else return null;
+    }
+
+    @PutMapping("/films")
+    public Film updateFilm(@Valid @RequestBody Film film) throws ValidEx {
+        if (validateFilmData(film)) {
+            films.put(film.getId(), film);
+            log.info("Данные фильма обновлены");
+            return film;
+        } else return null;
     }
 
     @GetMapping("/films")
@@ -43,4 +41,19 @@ public class FilmController {
         log.info("Получен запрос на получение списка фильмов");
         return new ArrayList<>(films.values());
     }
+
+    public boolean validateFilmData(Film film) throws ValidEx {
+        if (film.getName().isBlank()) {
+            throw new ValidEx("Название фильма не может быть пустым");
+        } else if (film.getDescription().length() > 200) {
+            throw new ValidEx("Описание фильма не может превышать 200 знаков");
+        } else if (film.getRealeseDate().after(LIMITE_DATE) | !film.getRealeseDate().equals(LIMITE_DATE)) {
+            throw new ValidEx("Дата релиза фильма не может быть раньше 28 декабря 1895 года");
+        } else if (film.getDuration().isNegative() | film.getDuration().isZero()) {
+            throw new ValidEx("Длительность фильма должна быть больше нуля");
+        } else {
+            return true;
+        }
+    }
 }
+
